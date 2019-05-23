b67eb40cdd24167715dc8cfb024fea39fb7b24a4
MIGUEL GUZMAN <miguel.guzman@dicipa.com.mx>
2019-05-23 12:52:17 -0500

    Material Table initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import MaterialTable from './lib/MaterialTable';
+const columnsArray = [{label:"Country",value:"country"},{label:"Language",value:"language"},
+{label:"Continent",value:"continent"},{label:"Capital",value:"capital"}
+];
+const itemList = [{id:"mx",country:"Mexico",language:"Spanish",continent:"American",capital:"Mexico City"},
+{id:"usa",country:"USA",language:"English",continent:"American",capital:"Washington DC"},
+{id:"ger",country:"Germany",language:"German",continent:"Europe",capital:"Berlin"},
+{id:"spa",country:"Spain",language:"Spanish",continent:"Europe",capital:"Madrid"},
+{id:"can",country:"Canada",language:"French,English",continent:"American",capital:"Ottawa"},
+{id:"uru",country:"Uruguay",language:"Spanish",continent:"American",capital:"Montevideo"},
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+]; 
+ 
+ReactDOM.render( 
+    <MaterialTable columnsArray={columnsArray}
+        itemsList ={itemList} defaultColumns={["country","language"]} numberColumnLabel={"#"} />
+    , document.getElementById('root'));
+
+
